29fc63bd615a355992cffb7eba1ba2678a14dafb
Garrett Hale <gthkeno@gmail.com>
2017-08-05 11:35:29 -0500

    Using gnu++11

--- buildit.sh
+++ buildit.sh
@@ -1,7 +1,7 @@
 echo "Building..."
 mkdir build
 cd build
-g++ -c -Wall -std=gnu++14 -I../src ../src/*.cpp &&\
+g++ -c -Wall -std=gnu++11 -I../src ../src/*.cpp &&\
 cd .. &&\
 g++ -o gert-wavefrontc build/*.o &&\
 echo "Done!"
